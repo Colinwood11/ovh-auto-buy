0148e84f88763fb3740197824083e98cb32f0d1f
Colin <admin@colinwood.pub>
2024-11-10 03:22:43 +0100

    feat: add option to set datacenters

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Automatically buys OVH Eco Servers (Docker)
+
 Go source code modified from: https://blog.yessure.org/index.php/archives/203
 
 To generate token:
+
 - IE: `https://eu.api.ovh.com/createToken/index.cgi?GET=/*&PUT=/*&POST=/*&DELETE=/*`
 - CA: `https://ca.api.ovh.com/createToken/index.cgi?GET=/*&PUT=/*&POST=/*&DELETE=/*`
 
 To get Eco planCode and options:
+
 - IE: https://eu.api.ovh.com/v1/order/catalog/public/eco?ovhSubsidiary=IE
 - CA: https://ca.api.ovh.com/v1/order/catalog/public/eco?ovhSubsidiary=CA
 
 ## Docker
+
 ```bash
 docker run -e APP_KEY="ovh_appkey" \
            -e APP_SECRET="ovh_appsecret" \
@@ -20,27 +24,30 @@ docker run -e APP_KEY="ovh_appkey" \
            -e ZONE="ovh_zone" \
            -e PLANCODE="product_plancode" \
            -e OPTIONS="product_options" \
+           -e DATACENTERS="datacenters" \
            -e AUTOPAY=true \
            -e FREQUENCY=5 \
            katorly/ovh-auto-buy:latest
 ```
 
 ## docker-compose.yml
+
 ```yaml
 services:
   auto-buy:
     image: katorly/ovh-auto-buy:latest
     container_name: ks-buy
     environment:
-      APP_KEY: "ovh_appkey"             # OVH的应用key
-      APP_SECRET: "ovh_appsecret"       # OVH的应用secret
-      CONSUMER_KEY: "ovh_consumerkey"   # OVH的消费者key
-      REGION: "ovh_region"              # 区域设置为, e.g. ovh-eu
-      TG_TOKEN: "telegram_bot_token"    # 你的Telegram Bot Token
+      APP_KEY: "ovh_appkey" # OVH的应用key
+      APP_SECRET: "ovh_appsecret" # OVH的应用secret
+      CONSUMER_KEY: "ovh_consumerkey" # OVH的消费者key
+      REGION: "ovh_region" # 区域设置为, e.g. ovh-eu
+      TG_TOKEN: "telegram_bot_token" # 你的Telegram Bot Token
       TG_CHATID: "telegram_your_chatid" # 你希望发送消息的Telegram Chat ID
-      ZONE: "ovh_zone"                  # OVH子公司区域设置, e.g. IE
-      PLANCODE: "product_plancode"      # 需要购买的产品的planCode, e.g. 25skleb01
-      OPTIONS: "product_options"        # 选择的配置, e.g. bandwidth-300-25skle,ram-32g-ecc-2400-25skle,softraid-2x450nvme-25skle
-      AUTOPAY: true                     # 是否自动支付, e.g. true
-      FREQUENCY: 5                      # 检查频率单位为秒, e.g. 5
+      ZONE: "ovh_zone" # OVH子公司区域设置, e.g. IE
+      PLANCODE: "product_plancode" # 需要购买的产品的planCode, e.g. 25skleb01
+      OPTIONS: "product_options" # 选择的配置, e.g. bandwidth-300-25skle,ram-32g-ecc-2400-25skle,softraid-2x450nvme-25skle
+      DATACENTERS: "" # 选择的数据中心，留空全都要 e.g. fr,gra
+      AUTOPAY: true # 是否自动支付, e.g. true
+      FREQUENCY: 5 # 检查频率单位为秒, e.g. 5
 ```
--- main.go
+++ main.go
@@ -1,257 +1,272 @@
 package main
 
-/** 
+/**
  * Source code modified from: https://blog.yessure.org/index.php/archives/203
  */
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "strconv"
-    "strings"
-    "time"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
-    "github.com/ovh/go-ovh/ovh"
+	"github.com/ovh/go-ovh/ovh"
 )
 
 var (
-    appKey      = os.Getenv("APP_KEY")			// OVH的应用key
-    appSecret   = os.Getenv("APP_SECRET")		// OVH的应用secret
-    consumerKey = os.Getenv("CONSUMER_KEY")		// OVH的消费者key
-    region      = os.Getenv("REGION")			// 区域设置为, e.g. ovh-eu
-    tgtoken     = os.Getenv("TG_TOKEN")			// 你的Telegram Bot Token
-    tgchatid    = os.Getenv("TG_CHATID")		// 你希望发送消息的Telegram Chat ID
-    zone        = os.Getenv("ZONE")				// OVH子公司区域设置, e.g. IE
-    plancode    = os.Getenv("PLANCODE")         // 需要购买的产品的planCode, e.g. 25skleb01
-    optionsenv  = os.Getenv("OPTIONS")          // 选择的配置用逗号分隔, e.g. bandwidth-300-25skle,ram-32g-ecc-2400-25skle,softraid-2x450nvme-25skle
-    autopay     = os.Getenv("AUTOPAY")          // 是否自动支付, e.g. true
-	frequency	= os.Getenv("FREQUENCY")		// 检查频率单位为秒, e.g. 5
+	appKey            = os.Getenv("APP_KEY")                         // OVH的应用key
+	appSecret         = os.Getenv("APP_SECRET")                      // OVH的应用secret
+	consumerKey       = os.Getenv("CONSUMER_KEY")                    // OVH的消费者key
+	region            = os.Getenv("REGION")                          // 区域设置为, e.g. ovh-eu
+	tgtoken           = os.Getenv("TG_TOKEN")                        // 你的Telegram Bot Token
+	tgchatid          = os.Getenv("TG_CHATID")                       // 你希望发送消息的Telegram Chat ID
+	zone              = os.Getenv("ZONE")                            // OVH子公司区域设置, e.g. IE
+	plancode          = os.Getenv("PLANCODE")                        // 需要购买的产品的planCode, e.g. 25skleb01
+	optionsenv        = os.Getenv("OPTIONS")                         // 选择的配置用逗号分隔, e.g. bandwidth-300-25skle,ram-32g-ecc-2400-25skle,softraid-2x450nvme-25skle
+	targetdatacenters = strings.Split(os.Getenv("DATACENTERS"), ",") // 选择的数据中心, e.g.
+	autopay           = os.Getenv("AUTOPAY")                         // 是否自动支付, e.g. true
+	frequency         = os.Getenv("FREQUENCY")                       // 检查频率单位为秒, e.g. 5
 )
 
-var bought = false                              // 是否已购买, 避免os.Exit(0)出错
+var bought = false // 是否已购买, 避免os.Exit(0)出错
 
 func runTask() {
 
-    client, err := ovh.NewClient(region, appKey, appSecret, consumerKey)
-    if err != nil {
-        log.Printf("Failed to create OVH client: %v\n", err)
-        return
-    }
+	client, err := ovh.NewClient(region, appKey, appSecret, consumerKey)
+	if err != nil {
+		log.Printf("Failed to create OVH client: %v\n", err)
+		return
+	}
 
-    var result []map[string]interface{}
-    err = client.Get("/dedicated/server/datacenter/availabilities", &result)
-    if err != nil {
-        log.Printf("Failed to get datacenter availabilities: %v\n", err)
-        return
-    }
+	var result []map[string]interface{}
+	err = client.Get("/dedicated/server/datacenter/availabilities", &result)
+	if err != nil {
+		log.Printf("Failed to get datacenter availabilities: %v\n", err)
+		return
+	}
 
-    foundAvailable := false
-    var fqn, planCode, datacenter string
+	foundAvailable := false
+	var fqn, planCode, datacenter string
 
-    for _, item := range result {
-        if item["planCode"] == plancode {
-            fqn = item["fqn"].(string)
-            planCode = item["planCode"].(string)
-            datacenters := item["datacenters"].([]interface{})
+	for _, item := range result {
+		if item["planCode"] == plancode {
+			fqn = item["fqn"].(string)
+			planCode = item["planCode"].(string)
+			datacenters := item["datacenters"].([]interface{})
 
-            for _, dcInfo := range datacenters {
-                dc := dcInfo.(map[string]interface{})
-                availability := dc["availability"].(string)
-                datacenter = dc["datacenter"].(string)
+			foundDatacenter := false
+			for _, dcInfo := range datacenters {
+				dc := dcInfo.(map[string]interface{})
+				availability := dc["availability"].(string)
+				datacenter = dc["datacenter"].(string)
 
-                fmt.Printf("FQN: %s\n", fqn)
-                fmt.Printf("Availability: %s\n", availability)
-                fmt.Printf("Datacenter: %s\n", datacenter)
-                fmt.Println("------------------------")
+				if len(targetdatacenters) > 0 {
+					for _, targetdc := range targetdatacenters {
+						if datacenter == targetdc {
+							foundDatacenter = true
+							break
+						}
+					}
 
-                if availability != "unavailable" {
-                    foundAvailable = true
-                    break
-                }
-            }
+					if !foundDatacenter {
+						break
+					}
 
-            if foundAvailable {
-                fmt.Printf("Proceeding to next step with FQN: %s Datacenter: %s\n", fqn, datacenter)
-                break
-            }
-        }
-    }
+				}
+				fmt.Printf("FQN: %s\n", fqn)
+				fmt.Printf("Availability: %s\n", availability)
+				fmt.Printf("Datacenter: %s\n", datacenter)
+				fmt.Println("------------------------")
 
-    if !foundAvailable {
-        log.Println("No record to buy")
-        return
-    }
+				if availability != "unavailable" {
+					foundAvailable = true
+					break
+				}
+			}
 
-    msg_available := fmt.Sprintf("🔥 有货: %s 在 %s 地区", plancode, datacenter)
-    sendTelegramMsg(tgtoken, tgchatid, msg_available)
+			if foundAvailable {
+				fmt.Printf("Proceeding to next step with FQN: %s Datacenter: %s\n", fqn, datacenter)
+				break
+			}
+		}
+	}
 
-    fmt.Println("Create cart")
-    var cartResult map[string]interface{}
-    err = client.Post("/order/cart", map[string]interface{}{
-        "ovhSubsidiary": zone,
-    }, &cartResult)
-    if err != nil {
-        log.Printf("Failed to create cart: %v\n", err)
-        return
-    }
+	if !foundAvailable {
+		log.Println("No record to buy")
+		return
+	}
 
-    cartID := cartResult["cartId"].(string)
-    fmt.Printf("Cart ID: %s\n", cartID)
+	msg_available := fmt.Sprintf("🔥 有货: %s 在 %s 地区", plancode, datacenter)
+	sendTelegramMsg(tgtoken, tgchatid, msg_available)
 
-    fmt.Println("Assign cart")
-    err = client.Post("/order/cart/"+cartID+"/assign", nil, nil)
-    if err != nil {
-        log.Printf("Failed to assign cart: %v\n", err)
-        return
-    }
+	fmt.Println("Create cart")
+	var cartResult map[string]interface{}
+	err = client.Post("/order/cart", map[string]interface{}{
+		"ovhSubsidiary": zone,
+	}, &cartResult)
+	if err != nil {
+		log.Printf("Failed to create cart: %v\n", err)
+		return
+	}
 
-    fmt.Println("Put item into cart")
-    var itemResult map[string]interface{}
-    err = client.Post("/order/cart/"+cartID+"/eco", map[string]interface{}{
-        "planCode":    planCode,
-        "pricingMode": "default",
-        "duration":    "P1M",
-        "quantity":    1,
-    }, &itemResult)
-    if err != nil {
-        log.Printf("Failed to add item to cart: %v\n", err)
-        return
-    }
+	cartID := cartResult["cartId"].(string)
+	fmt.Printf("Cart ID: %s\n", cartID)
 
-    var itemID string
-    if v, ok := itemResult["itemId"].(json.Number); ok {
-        itemID = v.String()
-    } else if v, ok := itemResult["itemId"].(string); ok {
-        itemID = v
-    } else {
-        log.Printf("Unexpected type for itemId, expected json.Number or string, got %T\n", itemResult["itemId"])
-        return
-    }
+	fmt.Println("Assign cart")
+	err = client.Post("/order/cart/"+cartID+"/assign", nil, nil)
+	if err != nil {
+		log.Printf("Failed to assign cart: %v\n", err)
+		return
+	}
 
-    fmt.Printf("Item ID: %s\n", itemID)
+	fmt.Println("Put item into cart")
+	var itemResult map[string]interface{}
+	err = client.Post("/order/cart/"+cartID+"/eco", map[string]interface{}{
+		"planCode":    planCode,
+		"pricingMode": "default",
+		"duration":    "P1M",
+		"quantity":    1,
+	}, &itemResult)
+	if err != nil {
+		log.Printf("Failed to add item to cart: %v\n", err)
+		return
+	}
 
-    fmt.Println("Checking required configuration")
-    var requiredConfig []map[string]interface{}
-    err = client.Get("/order/cart/"+cartID+"/item/"+itemID+"/requiredConfiguration", &requiredConfig)
-    if err != nil {
-        log.Printf("Failed to get required configuration: %v\n", err)
-        return
-    }
+	var itemID string
+	if v, ok := itemResult["itemId"].(json.Number); ok {
+		itemID = v.String()
+	} else if v, ok := itemResult["itemId"].(string); ok {
+		itemID = v
+	} else {
+		log.Printf("Unexpected type for itemId, expected json.Number or string, got %T\n", itemResult["itemId"])
+		return
+	}
 
-    dedicatedOs := "none_64.en"
-    var regionValue string
-    for _, config := range requiredConfig {
-        if config["label"] == "region" {
-            if allowedValues, ok := config["allowedValues"].([]interface{}); ok && len(allowedValues) > 0 {
-                regionValue = allowedValues[0].(string)
-            }
-        }
-    }
+	fmt.Printf("Item ID: %s\n", itemID)
 
-    configurations := []map[string]interface{}{
-        {"label": "dedicated_datacenter", "value": datacenter},
-        {"label": "dedicated_os", "value": dedicatedOs},
-        {"label": "region", "value": regionValue},
-    }
+	fmt.Println("Checking required configuration")
+	var requiredConfig []map[string]interface{}
+	err = client.Get("/order/cart/"+cartID+"/item/"+itemID+"/requiredConfiguration", &requiredConfig)
+	if err != nil {
+		log.Printf("Failed to get required configuration: %v\n", err)
+		return
+	}
 
-    for _, config := range configurations {
-        fmt.Printf("Configure %s\n", config["label"])
-        err = client.Post("/order/cart/"+cartID+"/item/"+itemID+"/configuration", map[string]interface{}{
-            "label": config["label"],
-            "value": config["value"],
-        }, nil)
-        if err != nil {
-            log.Printf("Failed to configure %s: %v\n", config["label"], err)
-            return
-        }
-    }
+	dedicatedOs := "none_64.en"
+	var regionValue string
+	for _, config := range requiredConfig {
+		if config["label"] == "region" {
+			if allowedValues, ok := config["allowedValues"].([]interface{}); ok && len(allowedValues) > 0 {
+				regionValue = allowedValues[0].(string)
+			}
+		}
+	}
 
-    fmt.Println("Add options")
-    options := strings.Split(optionsenv, ",")
+	configurations := []map[string]interface{}{
+		{"label": "dedicated_datacenter", "value": datacenter},
+		{"label": "dedicated_os", "value": dedicatedOs},
+		{"label": "region", "value": regionValue},
+	}
 
-    itemIDInt, _ := strconv.Atoi(itemID)
-    for _, option := range options {
-        err = client.Post("/order/cart/"+cartID+"/eco/options", map[string]interface{}{
-            "duration":    "P1M",
-            "itemId":      itemIDInt,
-            "planCode":    option,
-            "pricingMode": "default",
-            "quantity":    1,
-        }, nil)
-        if err != nil {
-            log.Printf("Failed to add option %s: %v\n", option, err)
-            return
-        }
-    }
+	for _, config := range configurations {
+		fmt.Printf("Configure %s\n", config["label"])
+		err = client.Post("/order/cart/"+cartID+"/item/"+itemID+"/configuration", map[string]interface{}{
+			"label": config["label"],
+			"value": config["value"],
+		}, nil)
+		if err != nil {
+			log.Printf("Failed to configure %s: %v\n", config["label"], err)
+			return
+		}
+	}
 
-    fmt.Println("Checkout")
-    var checkoutResult map[string]interface{}
-    err = client.Get("/order/cart/"+cartID+"/checkout", &checkoutResult)
-    if err != nil {
-        log.Printf("Failed to get checkout: %v\n", err)
-        return
-    }
+	fmt.Println("Add options")
+	options := strings.Split(optionsenv, ",")
 
-    autopayValue, err := strconv.ParseBool(autopay)
-    if err != nil {
-        fmt.Println("AUTOPAY value is invalid:", err)
-        return
-    }
+	itemIDInt, _ := strconv.Atoi(itemID)
+	for _, option := range options {
+		err = client.Post("/order/cart/"+cartID+"/eco/options", map[string]interface{}{
+			"duration":    "P1M",
+			"itemId":      itemIDInt,
+			"planCode":    option,
+			"pricingMode": "default",
+			"quantity":    1,
+		}, nil)
+		if err != nil {
+			log.Printf("Failed to add option %s: %v\n", option, err)
+			return
+		}
+	}
 
-    err = client.Post("/order/cart/"+cartID+"/checkout", map[string]interface{}{
-        "autoPayWithPreferredPaymentMethod": autopayValue,
-        "waiveRetractationPeriod":           true,
-    }, nil)
-    if err != nil {
-        log.Printf("Failed to checkout: %v\n", err)
-        return
-    }
-    log.Println("Ordered!")
+	fmt.Println("Checkout")
+	var checkoutResult map[string]interface{}
+	err = client.Get("/order/cart/"+cartID+"/checkout", &checkoutResult)
+	if err != nil {
+		log.Printf("Failed to get checkout: %v\n", err)
+		return
+	}
+
+	autopayValue, err := strconv.ParseBool(autopay)
+	if err != nil {
+		fmt.Println("AUTOPAY value is invalid:", err)
+		return
+	}
+
+	err = client.Post("/order/cart/"+cartID+"/checkout", map[string]interface{}{
+		"autoPayWithPreferredPaymentMethod": autopayValue,
+		"waiveRetractationPeriod":           true,
+	}, nil)
+	if err != nil {
+		log.Printf("Failed to checkout: %v\n", err)
+		return
+	}
+	log.Println("Ordered!")
 	bought = true
-    msg_ordered := fmt.Sprintf("🎉 订购成功: %s 在 %s 地区", datacenter, plancode)
-    sendTelegramMsg(tgtoken, tgchatid, msg_ordered)
-    os.Exit(0)
+	msg_ordered := fmt.Sprintf("🎉 订购成功: %s 在 %s 地区", datacenter, plancode)
+	sendTelegramMsg(tgtoken, tgchatid, msg_ordered)
+	os.Exit(0)
 }
 
 func sendTelegramMsg(botToken, chatID, message string) error {
-    url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
-    payload := map[string]string{
-        "chat_id": chatID,
-        "text":    message,
-    }
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
+	payload := map[string]string{
+		"chat_id": chatID,
+		"text":    message,
+	}
 
-    jsonData, err := json.Marshal(payload)
-    if err != nil {
-        return fmt.Errorf("error encoding JSON: %v", err)
-    }
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("error encoding JSON: %v", err)
+	}
 
-    resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
-    if err != nil {
-        return fmt.Errorf("error sending request: %v", err)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("error sending request: %v", err)
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return fmt.Errorf("received non-OK response status: %v", resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("received non-OK response status: %v", resp.Status)
+	}
 
-    return nil
+	return nil
 }
 
 func main() {
-    freq, err := strconv.Atoi(frequency)
-    if err != nil {
-        fmt.Println("Error converting frequency:", err)
-        return
-    }
-    for {
-        if (bought == false) {
+	freq, err := strconv.Atoi(frequency)
+	if err != nil {
+		fmt.Println("Error converting frequency:", err)
+		return
+	}
+	for {
+		if bought == false {
 			runTask()
 		}
-        time.Sleep(time.Duration(freq) * time.Second)
-    }
+		time.Sleep(time.Duration(freq) * time.Second)
+	}
 }
